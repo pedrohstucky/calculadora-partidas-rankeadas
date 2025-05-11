253961c365fd658a11b9c4ca8ab1dc89a3be6f5c
pedrohstucky <pedrostucky@gmail.com>
2025-05-10 22:50:43 -0300

    Project v1

---
+++ README.md
@@ -0,0 +1,34 @@
+# Calculadora de Partidas Rankeadas - DIO
+
+Este projeto é uma calculadora simples de partidas ranqueadas desenvolvida em **JavaScript**.
+O objetivo é receber o número de vitórias e derrotas de um jogador e determinar o nível do herói com base em suas vitórias.
+
+### Lógica da Classificação de Nível
+
+Se vitórias for menor do que 10 = Ferro  
+Se vitórias for entre 11 e 20 = Bronze  
+Se vitórias for entre 21 e 50 = Prata  
+Se vitórias for entre 51 e 80 = Ouro  
+Se vitórias for entre 81 e 90 = Diamante  
+Se vitórias for entre 91 e 100= Lendário  
+Se vitórias for maior ou igual a 101 = Imortal  
+
+## Como Executar
+
+1. Clone o repositório:
+
+```bash
+git clone https://github.com/pedrohstucky/calculadora-partidas-rankeadas.git
+cd calculadora-partidas-rankeadas
+```
+2. Instale a dependência do prompt-sync:
+```bash
+npm install prompt-sync
+```
+
+3. Execute o projeto:
+```bash
+node src/index.js
+```
+
+Code with ☕️ by Pedro H. Stucky
--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log("Initial config")
+const prompt = require('prompt-sync')()
+
+function getHeroWins(victories, defeats){
+    victories = parseInt(prompt("Digite a quantidade de vitórias do seu Herói: "))
+    defeats = parseInt(prompt("Digite a quantidade de derrotas do seu Herói: "))
+    return victories - defeats
+}
+
+function setHeroLevel(victoryBalance){
+    let level = ""
+
+    switch (true){
+        case victoryBalance <= 10:
+            level = "Ferro"
+            break
+        case victoryBalance <= 20:
+            level = "Bronze"
+            break
+        case victoryBalance <= 50:
+            level = "Prata"
+            break
+        case victoryBalance <= 80:
+            level = "Ouro"
+            break
+        case victoryBalance <= 90:
+            level = "Diamante"
+            break
+        case victoryBalance <= 100:
+            level = "Lendário"
+            break
+        default:
+            level = "Imortal"
+            break
+    }
+
+    return level
+}
+
+function main(){
+    let victoryBalance = getHeroWins()
+    let heroLevel = setHeroLevel(victoryBalance)
+    console.log(`O Herói tem um saldo de ${victoryBalance} vitórias e está no nível ${heroLevel}`)
+}
+
+main()
